f428cea0265c66574d478772a081e8e845ba6a61
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-02-03 10:37:04 +0100

    Update DNB_Testabfrage.py

--- DNB_Testabfrage.py
+++ DNB_Testabfrage.py
@@ -74,11 +74,15 @@ if st.button('Los!', key='push2'):
      records = response.find_all('record')
      records_marc = response.find_all('record', {'type':'Bibliographic'})
      gndm = response.find_all('record', {'type':'Authority'})
-     numberofrecords = response.find_all('numberofrecords')
-     test = numberofrecords[0].text
-     test2 = test.text
-     st.write(test)
-     st.write(test2)
+
+else:
+     st.write('Bitte wählen Sie Katalog und Metadatenformat und geben Sie einen Suchbegriff ein')
+
+numberofrecords = response.find_all('numberofrecords')
+test = numberofrecords[0].text
+test2 = test.text
+st.write(test)
+st.write(test2)
      #vorschau = records[0]
      #st.write("Gefundene Treffer:", numberofrecords)
      #st.write(" ")
@@ -86,7 +90,4 @@ if st.button('Los!', key='push2'):
      #st.write(vorschau.prettify())
      #st.write("")
      #st.write(" - Ende der Vorschau - ")  
-
-else:
-     st.write('Bitte wählen Sie Katalog und Metadatenformat und geben Sie einen Suchbegriff ein')
 
