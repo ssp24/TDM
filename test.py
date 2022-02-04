a93134e4e40070ed23bf9d93be7d6a1ede9b07eb
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-02-04 13:26:34 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,5 +1,5 @@
 import streamlit as st
 
-searchterm = st.text_input('Suchbegriff:', placeholder="Bitte Suchbegriff eingeben")
+searchterm = st.text_input('Suchbegriff:', placeholder="Bitte Suchbegriff eingeben", default_value="test")
 
 st.write(searchterm)
