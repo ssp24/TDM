cbb8970176258644ac32523e7212ede5d2dc2b7c
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-02-04 13:13:57 +0100

    Update DNB_Testabfrage.py

--- DNB_Testabfrage.py
+++ DNB_Testabfrage.py
@@ -22,10 +22,10 @@ st.markdown("Hier können Sie die SRU-Schnittstelle der Deutschen Nationalbiblio
         "muss die Reihenfolge bei Eingaben und Buttonklicks eingehalten werden. Im Anschluss können Sie sich eine "
         "gekürzte tabellarische Darstellung Ihrer Anfrage ansehen und diese als XML- oder CSV-Datei speichern. ")
         
-st.markdown("**Bitte beachten Sie**: "
+st.markdown("**Bitte beachten Sie**:  \n"
             "* Dieses Tutorial dient als Einstieg. Aus Performance-Gründen werden jeweils "
             "immer nur die **ersten 100 Treffer** Ihrer Anfrage ausgegeben. "
-            "* Die Metadatenformate enthalten "
+            "* Die Metadatenformate enthalten  \n "
             "unterschiedliche Informationen. Die Ausgabetabellen und -dateien variieren daher entsprechend in der "
             "Anzahl enthaltener Elemente und Informationen.")
 
