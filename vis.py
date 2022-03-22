ec6fa377b02b1cc7651bec4f79e58ff462128bfd
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-03-22 15:47:54 +0100

    Update vis.py

--- vis.py
+++ vis.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import plotly.express as px
 import plotly.figure_factory as ff
 import numpy as np
 from wordcloud import WordCloud
@@ -6,6 +7,6 @@ import matplotlib.pyplot as plt
 
 data = pd.read_json("data/hitlist.json")
 
-fig = ff.sunburst(data, path=['Parent_no', 'Fachgebiet'], values='Results')
+fig = px.sunburst(data, path=['Parent_no', 'Fachgebiet'], values='Results')
 
 st.plotly_chart(fig, use_container_width=True)
