bfffb90fc2cf91ae22fd62ecbd04d95ea97c5e57
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-03-22 16:12:27 +0100

    Update vis.py

--- vis.py
+++ vis.py
@@ -8,11 +8,11 @@ import matplotlib.pyplot as plt
 
 data = pd.read_json("data/hitlist.json")
 
-fig = px.sunburst(data, path=['Parent_no', 'Fachgebiet'], values='Results')
+fig = px.sunburst(data, path=['Parent_no', 'Fachgebiet'], values='Results', height=600)
 
 st.plotly_chart(fig, use_container_width=True)
 
-st.subheading("Darstellung Sachgruppen DDC")
+st.subheader("Darstellung Sachgruppen DDC")
 
 fig2 = px.sunburst(data, path=['Parent_title','Fachgebiet'], values='Results',
                   color='Results', hover_data=['DDC'],
@@ -23,3 +23,18 @@ fig2 = px.sunburst(data, path=['Parent_title','Fachgebiet'], values='Results',
 fig2.update_traces(insidetextorientation='radial')
 
 st.plotly_chart(fig2, use_container_width=True)
+
+
+st.header("Wordcloud")
+
+ddcdata = data.set_index('DDC').to_dict()['Results']
+worddata = data.set_index('Fachgebiet').to_dict()['Results']
+
+wc = WordCloud(background_color="white").generate_from_frequencies(ddcdata)
+wc2 = WordCloud(background_color="white", width=800, height=400).generate_from_frequencies(worddata)
+
+plt.figure(figsize=(10, 10))
+plt.imshow(wc2, interpolation="bilinear")
+plt.axis("off")
+
+st.pyplot(wc2)
