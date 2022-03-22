9848d6db771168f305f76cbabc0d1c6f29c390c6
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-03-22 16:01:00 +0100

    Update vis.py

--- vis.py
+++ vis.py
@@ -11,3 +11,14 @@ data = pd.read_json("data/hitlist.json")
 fig = px.sunburst(data, path=['Parent_no', 'Fachgebiet'], values='Results')
 
 st.plotly_chart(fig, use_container_width=True)
+
+
+fig2 = px.sunburst(data, path=['Parent_title','Fachgebiet'], values='Results',
+                  color='Parent_no', hover_data=['DDC'],
+                  color_continuous_scale='YlGn',
+                  color_continuous_midpoint=np.average(data['Parent_no'], weights=data['Results']),
+                  height=800)
+
+fig2.update_traces(insidetextorientation='radial')
+
+st.plotly_chart(fig2, use_container_width=True)
