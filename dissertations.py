6e2ac8a98d41f637c9c2a2f3a1ec7e2a486db2a6
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-04-14 23:38:11 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -6,7 +6,7 @@ import numpy as np
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
 
-data = pd.read_hdf("data/online_diss_cleaned.h5")
+data = pd.read_hdf("https://www.bygenius.eu/data/online_diss_cleaned.h5")
 #data = pd.read_excel("data/DDCall.xlsx")
 dnbcolor = ['#FEFEFE', '#2499ff', '#f33930', '#b6c73f', '#ffd44d',
             '#3cb8f6', '#f9852e', '#e3d98f', '#000000', '#01be00']
