df955c8db3242fedad47b68b98dd4193ef697a1e
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-04-21 19:35:26 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -13,8 +13,8 @@ test = h5py.File("data/testdf.h5")
 #data = pd.read_hdf("data/online_diss_cleaned.h5")
 test2 = h5py.File("data/online_diss_cleaned.h5")
 data = pd.DataFrame(test2)
-data['Results'] = data['Results'].astype(int)
-#data
+#data['Results'] = data['Results'].astype(int)
+data
 
 
 dnbcolor = ['#FEFEFE', '#2499ff', '#f33930', '#b6c73f', '#ffd44d',
@@ -36,12 +36,12 @@ st.subheader("Anzahl der Online-Hochschulschriften im Bestand nach Jahren: ")
 st.info("INFO: Es werden die Daten für die Jahre 1990 bis 2022 (laufend) dargestellt." ) 
 
 #Jahre: 
-data = data[data['Year'].notna()]
-data = data.astype({'Year':'int'})
-data = data[(data['Year'] >= 1900) & (data['Year'] <= 2100)]
+#data = data[data['Year'].notna()]
+#data = data.astype({'Year':'int'})
+#data = data[(data['Year'] >= 1900) & (data['Year'] <= 2100)]
 
-s = data['Year'].value_counts()[:33].sort_index()
-fig = px.bar(s, labels={'index':'Jahr', 'value':'Anzahl'}, color='value', height=500)
-st.plotly_chart(fig, use_container_width=True)
+#s = data['Year'].value_counts()[:33].sort_index()
+#fig = px.bar(s, labels={'index':'Jahr', 'value':'Anzahl'}, color='value', height=500)
+#st.plotly_chart(fig, use_container_width=True)
 
 
