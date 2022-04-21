e77e65fb6a2ad0f03a80ab7426cff6d24d414052
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-04-21 22:36:29 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -12,7 +12,7 @@ test = h5py.File("data/testdf.h5")
 
 #data = pd.read_hdf("data/online_diss_cleaned.h5")
 test2 = h5py.File("data/online_diss_cleaned.h5")
-data = pd.DataFrame(test2)
+data = pd.DataFrame(test2, key="df")
 #data['Results'] = data['Results'].astype(int)
 st.dataframe(data)
 
