4627bcd992b4fe8b8dfc544358dec1ff1fa3c673
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-08-02 16:51:35 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -12,7 +12,7 @@ import plotly.express as px
 #data = pd.read_json("https://www.bygenius.eu/data/diss_06-2022_cleaned.json")
 
 dissyears = pd.read_json("data/diss_years.json")
-st.dataframe(dissyears)
+#st.dataframe(dissyears)
 
 
 dnbcolor = ['#FEFEFE', '#2499ff', '#f33930', '#b6c73f', '#ffd44d',
@@ -42,16 +42,16 @@ st.plotly_chart(fig, use_container_width=True)
 
 st.info("INFO: Es werden die Daten für die Jahre 1990 bis 2022 (laufend) dargestellt. " ) 
 
-dissyears2 = pd.read_json("data/diss_years.json")
-dissyears2["url"] = "https://portal.dnb.de/opac.htm?method=simpleSearch&cqlMode=true&query=catalog=dnb.hss+location=onlinefree+jhr="+dissyears2["year"].astype(str)
+dissyears2 = pd.read_json("data/dissyears.json")
+dissyears2["url"] = "https://portal.dnb.de/opac.htm?method=simpleSearch&cqlMode=true&query=catalog=dnb.hss+location=onlinefree+jhr="+dissyears2[years"].astype(str)
 dissyears2
 
-fig2 = px.bar(dissyears, x="year", y = "count", labels={'years':'Jahr', 'count':'Anzahl'}, color='count', height=500)
-update = (len(dissyears2["year"]))
+fig2 = px.bar(dissyears, x="years", y = "count", labels={'years':'Jahr', 'count':'Anzahl'}, color='count', height=500)
+update = (len(dissyears2["years"]))
 
 for i in range (0,update):     
     fig2.add_annotation(      
-                x = dissyears2["year"].values[i],
+                x = dissyears2["years"].values[i],
                 y = dissyears2["count"].values[i],
                 textangle = 90,
                 text = f"""<a href="{dissyears2["url"].values[i]}" target="_blank">{dissyears2["count"].values[i]}</a>""",
