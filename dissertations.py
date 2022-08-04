8fe0c1c433e7f934b607c3a4c1fc1c461ce0df1c
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-08-04 19:09:22 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -11,7 +11,7 @@ with st.sidebar:
     st.subheader("Auswahlmenü")
     visual = st.selectbox(
      'Bitte wählen Sie die Anzeige: ',
-     ('Publikationsjahre', 'Verteilung nach Fächern', 'Publikationsorte', index="Publikationsjahre")
+     ('Publikationsjahre', 'Verteilung nach Fächern', 'Publikationsorte', 1)
     )
     st.write('Momentan angezeigt:', visual)
 
