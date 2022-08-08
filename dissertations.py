e292adae72e45f566f55a16d1e5028a7deb1db8e
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-08-08 14:09:26 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -90,9 +90,10 @@ elif visual == "Verteilung nach Fächern":
      
     
     with col1:
-        st.caption("")
-        st.markdown("[![Foo](https://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png)](http://creativecommons.org/licenses/by-nc-nd/3.0/)") 
+        st.write("")
         st.write()
+        st.markdown("[![Foo](https://i.creativecommons.org/l/by-nc-nd/3.0/88x31.png)](http://creativecommons.org/licenses/by-nc-nd/3.0/)") 
+        
         
     with col2:     
         st.caption("This work is licensed under a Creative Commons Attribution-Noncommercial-No Derivative Works 3.0 Unported License "
