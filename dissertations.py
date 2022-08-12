8426f19d533d61d93714bbe7eb69675fb96d8c81
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-08-12 14:35:25 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -143,6 +143,29 @@ if visual == "Übersicht":
 
         st.markdown(htmlstr, unsafe_allow_html=True)
 
+   
+    twocol1, twocol2, twocol3 = st.columns(3)
+    
+    with twocol1: 
+        wch_colour_box = (100,204,302)
+        fontsize = 18
+        sline = "Sozialwissenschaften"
+        i = pub_300
+
+        htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, {wch_colour_box[1]}, {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, {wch_colour_font[1]}, {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+        st.markdown(htmlstr, unsafe_allow_html=True)
+        
 
 #------ PUBLIKATIONSJAHRE --------------
 
