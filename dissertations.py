69f641de0799c228d73233319117955cd29cd3d0
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-08-12 14:03:57 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -8,34 +8,6 @@ import plotly.graph_objects as go
 import plotly.express as px
 
 
-wch_colour_box = (0,204,102)
-wch_colour_font = (0,0,0)
-fontsize = 18
-valign = "left"
-iconname = "fas fa-asterisk"
-sline = "Observations"
-#lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
-i = 123
-
-htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
-                                              {wch_colour_box[1]}, 
-                                              {wch_colour_box[2]}, 0.75); 
-                        color: rgb({wch_colour_font[0]}, 
-                                   {wch_colour_font[1]}, 
-                                   {wch_colour_font[2]}, 0.75); 
-                        font-size: {fontsize}px; 
-                        border-radius: 7px; 
-                        padding-left: 12px; 
-                        padding-top: 18px; 
-                        padding-bottom: 18px; 
-                        line-height:25px;'>
-                        <i class='{iconname} fa-xs'></i> {i}
-                        </style><BR><span style='font-size: 14px; 
-                        margin-top: 0;'>{sline}</style></span></p>"""
-
-st.markdown(htmlstr, unsafe_allow_html=True)
-
-
 
 # ---- SIDEBAR ----- 
 
@@ -69,8 +41,6 @@ st.write(" Das Datenset wird alle 3 Monate aktualisiert."  )
 
 if visual == "Übersicht":
 
-    #st.subheader('Statistik')
-
     st.write("")
 
 
@@ -80,7 +50,33 @@ if visual == "Übersicht":
 
     st.metric(label="Anzahl freie online Dissertationen", value=all_ofd, delta=growth)
     st.caption("Seit der letzten Aktualisierung des Datensets im März 2022.")
+  
+    wch_colour_box = (0,204,102)
+    wch_colour_font = (0,0,0)
+    fontsize = 18
+    valign = "left"
+    iconname = "fas fa-asterisk"
+    sline = "Observations"
+    #lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
+    i = 123
 
+    htmlstr = f"""<p style='background-color: rgb({wch_colour_box[5]}, 
+                                              {wch_colour_box[1]}, 
+                                              {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, 
+                                   {wch_colour_font[1]}, 
+                                   {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+    st.markdown(htmlstr, unsafe_allow_html=True)
 
 
 #------ PUBLIKATIONSJAHRE --------------
