765120bc25178e454362aad8076903e37b87933c
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-08-12 14:16:54 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -79,6 +79,24 @@ if visual == "Übersicht":
         sline = "Informatik, Information & Wissen, allgemeine Werke"
         #lnk = '<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.12.1/css/all.css" crossorigin="anonymous">'
         i = pub_000
+        
+        htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, 
+                                              {wch_colour_box[1]}, 
+                                              {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, 
+                                   {wch_colour_font[1]}, 
+                                   {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+
+        st.markdown(htmlstr, unsafe_allow_html=True)
     
     
     with col2: 
