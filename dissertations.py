a3a523b9404c1aa5163f92e9ab3aff2a89c30468
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-08-23 17:59:53 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -235,7 +235,7 @@ if visual == "Übersicht":
     threecol1, threecol2, threecol3 = st.columns(3)
       
     with threecol1: 
-        wch_colour_box = (255,212,77)
+        wch_colour_box = (255,200,31)
         fontsize = 18
         sline = "Technik"
         i = pub_600
@@ -260,6 +260,26 @@ if visual == "Übersicht":
         fontsize = 18
         sline = "Künste & Freizeit und Erholung"
         i = pub_700
+        
+        htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, {wch_colour_box[1]}, {wch_colour_box[2]}, 0.75); 
+                        color: rgb({wch_colour_font[0]}, {wch_colour_font[1]}, {wch_colour_font[2]}, 0.75); 
+                        font-size: {fontsize}px; 
+                        border-radius: 7px; 
+                        padding-left: 12px; 
+                        padding-top: 18px; 
+                        padding-bottom: 18px; 
+                        line-height:25px;'>
+                        <i class='{iconname} fa-xs'></i> {i}
+                        </style><BR><span style='font-size: 14px; 
+                        margin-top: 0;'>{sline}</style></span></p>"""
+     
+        st.markdown(htmlstr, unsafe_allow_html=True)
+        
+    with threecol3: 
+        wch_colour_box = (0,70,196)
+        fontsize = 18
+        sline = "Literatur"
+        i = pub_800
         
         htmlstr = f"""<p style='background-color: rgb({wch_colour_box[0]}, {wch_colour_box[1]}, {wch_colour_box[2]}, 0.75); 
                         color: rgb({wch_colour_font[0]}, {wch_colour_font[1]}, {wch_colour_font[2]}, 0.75); 
