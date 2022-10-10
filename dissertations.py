6e2788e1ea02cf79c006f6c96cb79f4e54320b30
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-10-10 21:37:58 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -560,8 +560,10 @@ elif visual=="Info":
             " welche Anpassungen vorgenommen und welche Daten berücksichtigt wurden, bzw. welche Daten nicht berücksichtigt werden konnten.") 
         
     st.write("Übersicht nach Fachgebieten:") 
-    st.write("Im Datensatz der DNB enthalten sind ingesamt zum aktuellen Stand (siehe Sidebar) 288.677 Datensätze. Von diesen verfügen 288.123 "
-            "über einen Hochschulschriftenvermerk. ") 
+    st.markdown("Im Datensatz der DNB enthalten sind ingesamt zum aktuellen Stand (siehe Sidebar) 288.677 Datensätze. Von diesen verfügen 288.123 "
+            "über einen Hochschulschriftenvermerk [link](https://www.dnb.de/DE/Service/Hilfe/HilfeNP/_content/Ablieferung/Monografienn/hochschulschriftenvermerkAblieferung8_akk.html). Bei den 554 Datensätzen ohne Hochschulschriftenvermerk handelt es sich zumeist um übergeordnete "
+            "Datensätze monographischer Reihen, in denen entsprechende Hochschulschriften erschienen sind. Diese Datensätze wurden für die weitere "
+            "Darstellung des Sets entsprechend nicht berücksichtigt." ) 
 
     
     
