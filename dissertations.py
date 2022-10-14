9f56c0a81f89259d8644ebb4280205f03bbf9c7f
ssp24 <62755563+ssp24@users.noreply.github.com>
2022-10-14 13:47:07 +0200

    Update dissertations.py

--- dissertations.py
+++ dissertations.py
@@ -17,6 +17,8 @@ dissyears2 = pd.read_json("data/dissyears.json")
 
 # ---- SIDEBAR ----- 
 
+st.subheader('"Freie Online-Hochschulschriften" in der DNB')
+
 with st.sidebar:
     st.subheader("Auswahlmenü")
     visual = st.selectbox(
